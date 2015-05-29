ca550ee6698055558763c1b92946fe6f21431fe8
Taylor Gerring <taylor.gerring@gmail.com>
2015-05-29 13:46:10 -0500

    New format for geth-latest

--- geth-latest/tools/chocolateyInstall.ps1
+++ geth-latest/tools/chocolateyInstall.ps1
@@ -1,27 +1,15 @@
 $packageName    = 'geth-latest'
-$source = "https://build.ethdev.com/builds/Windows%20Go%20develop%20branch/Geth-Win64-20150518194305-0.9.22-60--6c2ad7b.7z"
+$url = "https://build.ethdev.com/builds/Windows%20Go%20develop%20branch/Geth-Win64-latest.7z"
 
-$destination = "$env:chocolateyPackageFolder\tools"
-$dlname = "Geth-Win64-latest.7z"
-$7za = "$env:chocolateyPackageFolder\..\7zip.commandline\tools\7za.exe"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$shortcutFile = Join-Path $env:userprofile "Desktop\$packageName.bat"
 
-$shortcutDir = [Environment]::GetFolderPath("Desktop")
-$shortcutFile = "$packageName.lnk"
+# Download and extract package
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+# Prevent shim from being created in bin directory
+"shimgen ignore" | Set-Content "$(Join-Path $toolsDir 'geth.exe.ignore')" -Encoding ASCII
 
-Write-Host Let me get the latest version for you. I am copying it to the folder:
-Write-Host $destination\$dlname 
-Get-ChocolateyWebFile 'geth' "$destination\$dlname" "$source"
+#$batContent = "$toolsDir\geth.exe console 2> $toolsDir\geth.log"
+$batContent = "$toolsDir\geth.exe console"
 
-& $7za x -y "-o$destination" $destination\$dlname
-
-Write-Host Cleaning up the downloaded file.
-Remove-Item $destination\$dlname
-
-Write-Host All done, you can start Geth from its install folder at
-Write-Host ---> $env:chocolateyPackageFolder\tools\geth.exe
-Write-Host Or run it from your Desktop
-
-Install-ChocolateyShortcut -shortcutFilePath "$shortcutDir\$shortcutFile" -targetPath "$env:chocolateyPackageFolder\tools\console.bat" -Description "Geth (Latest)"
-
-#"$env:chocolateyPackageFolder\tools\geth.exe console 2> $env:chocolateyPackageFolder\tools\geth.log" | Set-Content "$destination\console.bat" -Encoding ASCII
-"$env:chocolateyPackageFolder\tools\geth.exe console" | Set-Content "$destination\console.bat" -Encoding ASCII
+"$batContent"| Set-Content $shortcutFile -Encoding ASCII
--- geth-latest/tools/chocolateyUninstall.ps1
+++ geth-latest/tools/chocolateyUninstall.ps1
@@ -3,8 +3,8 @@ $packageName = 'geth-latest'
 Write-Host This only removes Geth, not any databases or keys!
 Write-Host For more information see https://github.com/ethereum/go-ethereum/wiki/Backup-%26-restore
 
-$shortcutDir = [Environment]::GetFolderPath("Desktop")
-$shortcutFile = "$packageName.lnk"
+$shortcutFile = Join-Path $env:userprofile "Desktop\$packageName.bat"
 
-if (Test-Path "$shortcutDir\$shortcutFile") {Remove-Item $shortcutDir\$shortcutFile} 
+
+if (Test-Path "$$shortcutFile") {Remove-Item $$shortcutFile} 
 
