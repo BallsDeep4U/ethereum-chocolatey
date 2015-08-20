12bf6922aec1c62bc70e9955a8d71a0c9c25f678
Taylor Gerring <taylor.gerring@gmail.com>
2015-08-21 00:09:59 +0200

    Bump version to 1.0.1.2

--- geth-stable/tools/chocolateyInstall.ps1
+++ geth-stable/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName    = 'geth-stable'
-$url = 'https://github.com/ethereum/go-ethereum/releases/download/v1.0.1.1/Geth-Win64-20150811104524-1.0.1-7fbf990.zip '
+$url = 'https://github.com/ethereum/go-ethereum/releases/download/v1.0.1.2/Geth-Win64-20150820212541-1.0.1-f0c7af0.zip'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $shortcutFile = Join-Path $env:userprofile "Desktop\$packageName.bat"
